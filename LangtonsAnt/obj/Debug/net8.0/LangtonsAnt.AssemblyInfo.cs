179fa076bf0b896f72134f13293537599b25624f
faiz_sejong <faiz@sju.ac.kr>
2024-01-10 13:35:45 +0900

    abc

--- LangtonsAnt/obj/Debug/net8.0/LangtonsAnt.AssemblyInfo.cs
+++ LangtonsAnt/obj/Debug/net8.0/LangtonsAnt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LangtonsAnt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a60367e9ad350af2f9f672366c12d30a367d35bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("LangtonsAnt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LangtonsAnt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
